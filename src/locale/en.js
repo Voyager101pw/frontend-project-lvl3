a463649dee961b5aa537896af9f3bcb9ea4401ea
Voyager101pw <voyager101sirius@gmail.com>
2022-04-30 03:28:12 +0500

    Step 6-7: Added modal window, fix code

--- src/locale/en.js
+++ src/locale/en.js
@@ -1,9 +1,30 @@
 const en = {
   translation: {
-    url: 'This must be a valid URL',
-    uniq: 'This URL should be unique',
-    parse: 'URL does not contain valid RSS entries!',
-    valid: 'RSS uploaded successfully',
+    success: 'RSS uploaded successfully',
+    errors: {
+      empty: 'Must not be empty',
+      url: 'Link must be a valid URL',
+      uniq: 'TRSS already exists',
+      parse: 'The resource does not contain valid RSS',
+      request: 'Network error',
+      modal: 'Modal window display error',
+    },
+    trans: {
+      title: 'RSS aggregator',
+      subTitle: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
+      label: 'RSS link',
+      button: 'Add',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+    },
+    cards: {
+      feeds: 'Feeds',
+      posts: 'Posts',
+      button: 'View',
+    },
+    modal: {
+      read: 'Read all',
+      close: 'Close',
+    },
   },
 };
 
