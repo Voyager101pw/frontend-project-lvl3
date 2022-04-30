06b08c4a1cb21bd8e81a7e919fb9afac41e3783e
Voyager101pw <voyager101sirius@gmail.com>
2022-04-30 21:17:31 +0500

    as1w4lxz

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 
@@ -15,6 +15,12 @@ module.exports = {
       filename: 'index.html',
     }),
   ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
 
   module: {
     rules: [
@@ -28,16 +34,4 @@ module.exports = {
       },
     ],
   },
-
-  devServer: {
-    historyApiFallback: true,
-    static: {
-      directory: path.join(path.resolve()),
-    },
-    hot: true,
-    open: true,
-    compress: true,
-    port: 8080,
-  },
-
 };
